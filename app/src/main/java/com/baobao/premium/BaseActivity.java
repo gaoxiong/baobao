16d1ebe2c0b29524bbb78372c3131f2d282ebae3
xgao <xgao@wandoujia.com>
2015-01-22 13:05:36 +0800

    Add sherlockAction

---
+++ app/src/main/java/com/baobao/premium/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.baobao.premium;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+/**
+ * Created by gaoxiong on 15-1-21.
+ */
+public abstract class BaseActivity extends SherlockFragmentActivity {
+}
